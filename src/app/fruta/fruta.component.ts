c9df89b00e273be3712a16fe231ce9d3ccf80a58
iscrack <isfenixrojo@mail.com>
2020-09-15 00:00:09 -0500

    Checando las propiedades y tipos de datos en angular

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'fruta',
-    templateUrl:'./fruta.component.html'
+    selector: 'fruta',
+    templateUrl: './fruta.component.html'
 })
 
-export class FrutaComponent
-{
-    public componente_fruta= 'Listado de Frutas';
-    public listado_frutas= 'Manzana, Pera, Mango y Naranja';
-    public nombre:string = "Israel Hernandez Vazquez";
+export class FrutaComponent {
+    public componente_fruta = 'Listado de Frutas';
+    public listado_frutas = 'Manzana, Pera, Mango y Naranja';
+    public nombre: string = "Israel Hernandez Vazquez";
+    public edad: number = 29;
+    public mayorDeEdad: boolean = true;
+    public trabajos: Array<any> = ['Carpintero', 'Albañil', 'Fontanero', 45];// El tipo any puede llevar cualquier cosa.
+    comodin:any = 30;
 }
