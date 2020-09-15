278c33ccaccb122ce1256cbd100a810bda9bea54
iscrack <isfenixrojo@mail.com>
2020-09-15 13:08:42 -0500

    Uso de directiva ngIf y evento click

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
-    selector:'empleado',
-    templateUrl:'./empleado.component.html'
+    selector: 'empleado',
+    templateUrl: './empleado.component.html'
 })
 
-export class EmpleadoComponent
-{
-    public componente_empleado= 'Lista de Empleados';
-    public listado_empleados= 'Ursula, Israel, Marco, Karla';
+export class EmpleadoComponent {
+    public componente_empleado = 'Lista de Empleados';
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+    public trabajadorExterno: boolean;
+
+    constructor() {
+        this.empleado = new Empleado("Israel Hernandez", 29, "Desarrollador", false);
+        this.trabajadores = [
+            new Empleado("Simon Moon Moon", 6, "Trainig", true),
+            new Empleado("Pelusa Pelu", 6, "Trainig", true),
+            new Empleado("Evaristo Maclovio", 30, "Vendedor", false),
+            new Empleado("Florentina Villa", 26, "RR.HH", false)
+        ];
+        this.trabajadorExterno = true;
+    }
+
+    ngOnInit() {
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor) {
+        this.trabajadorExterno = valor;
+    }
 }
