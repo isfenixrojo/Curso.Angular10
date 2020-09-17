be243f9c67803b073e920e511be97cdac3fda0b9
iscrack <isfenixrojo@mail.com>
2020-09-16 23:18:24 -0500

    Creacion y uso de servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { RopaService } from '../services/ropa.service'
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [RopaService]
 })
 export class HomeComponent implements OnInit {
 
   public titulo = "Pagina principal - Home"
-  constructor() { }
+  public listadoRopa: Array<string>;
+  public prendaAGuardar: string;
 
-  ngOnInit(): void {
+  constructor(
+    private _ropaSevice: RopaService
+  ) { }
+
+  ngOnInit() {
+    this.listadoRopa = this._ropaSevice.getRopa();
+    console.log(this.listadoRopa);
   }
-
+  guardarPrenda() {
+    this._ropaSevice.addRopa(this.prendaAGuardar);
+    this.prendaAGuardar = null;
+  }
+  eliminarPrenda(index: number) {
+    this._ropaSevice.deleteRopa(index);
+  }
 }
